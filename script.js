5038353473fbbac5b152a64147d77eb2705334d1
jeppegitrekt <jeppewest@msn.com>
2025-04-28 18:21:50 +0200

    Legge til func for humanChoice
    la til funksjon for humanChoice slik at computer
    og menneske kan spille mot hverandre

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function getComputerChoice() {
  let choices = ['Rock', 'Paper', 'Scissors'];
+ console.log(choices);
  return choices[Math.floor(Math.random() * choices.length)];
     }
+
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, paper or scissors?");
+    console.log(humanChoice);
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(getComputerChoice, getHumanChoice) {
+    
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
