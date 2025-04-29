344699bd83cb1dea327c1551133427786eb518a1
jeppegitrekt <jeppewest@msn.com>
2025-04-29 13:03:30 +0200

    Legge til ifs for playRound-func for tap og vinn
    la til forskjellige ifs for å gi feedback til
    spiller når de taper eller vinner.

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
+// generates one of three words, "rock, paper, scissors"
 function getComputerChoice() {
- let choices = ['Rock', 'Paper', 'Scissors'];
- console.log(choices);
+ let choices = ['Paper', 'Rock', 'Scissors'];
  return choices[Math.floor(Math.random() * choices.length)];
     }
 
-
+// creates prompt and returns user input
 function getHumanChoice() {
     let humanChoice = prompt("Rock, paper or scissors?");
     console.log(humanChoice);
     return humanChoice;
 }
 
+// variables to store score between human and computer
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(getComputerChoice, getHumanChoice) {
-    
-}
+function playRound(humanChoice, computerChoice) {
 
-const humanSelection = getHumanChoice().toLowerCase();
-const computerSelection = getComputerChoice();
+    if (humanChoice == 'rock' && computerChoice == 'Scissors') {
+        console.log("You win! The computer chose scissors!");
+    }
+    else if (humanChoice == 'paper' && computerChoice == 'Rock') {
+        console.log("You win! The computer chose rock!");
+    }
+    else if (humanChoice == 'scissors' && computerChoice == 'Paper') {
+        console.log("You win! The computer chose paper!");
+    }
+    else if (humanChoice == 'rock' && computerChoice == 'Paper') {
+        console.log("You lose. Computer chose paper!");
+    }
+    else if (humanChoice == 'paper' && computerChoice == 'Scissors') {
+        console.log("You lose. Computer chose scissors!");
+    }
+    else if (humanChoice == 'scissors' && computerChoice == 'Rock') {
+        console.log("You lose. Computer chose rock!");
+    }
+    else {
+        console.log("It's a tie! You both chose " + humanChoice + '.');
+    }
 
-playRound(humanSelection, computerSelection);
+    if (humanChoice > computerChoice) {
+        humanScore++;
+    }
+    else {
+        computerScore++;
+    }
+    console.log('Your score: ' + humanScore);
+    console.log('Computers score: ' + computerScore);
+    }
+
+
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
